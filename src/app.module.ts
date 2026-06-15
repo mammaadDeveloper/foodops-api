129a76bd5682b99e1011ed7e78192895246ef139
Mohammadreza <developerMammad@proton.me>
2026-06-16 00:50:51 +0330

    build: Install nestjs config.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './configs/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [appConfig],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
